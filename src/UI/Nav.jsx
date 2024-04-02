023ba5cdd32f3706c8adb1a371173c42ed83f346
brsekani <lawaltemidayo06@gmail.com>
2024-04-02 13:49:24 -0700

    Routing done with nav buttons

--- src/UI/Nav.jsx
+++ src/UI/Nav.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+
 import { LuLayoutDashboard } from "react-icons/lu";
 import { PiCardsBold } from "react-icons/pi";
 import { FaMoneyBills } from "react-icons/fa6";
@@ -7,6 +7,7 @@ import { FaFileInvoice } from "react-icons/fa";
 import { GoGoal } from "react-icons/go";
 import { MdOutlineAccountCircle } from "react-icons/md";
 import { BiSupport } from "react-icons/bi";
+import { NavLink, useLocation } from "react-router-dom";
 
 const navItems = [
   { icon: LuLayoutDashboard, text: "Dashboard" },
@@ -19,10 +20,9 @@ const navItems = [
 ];
 
 function Nav({ isHovered, setIsHovered, closeSidebar }) {
-  const [currentPage, setCurrentPage] = useState(1);
+  const location = useLocation().pathname.slice(1);
 
-  const handleClick = (index) => {
-    setCurrentPage(index);
+  const handleClick = () => {
     setIsHovered(false); // Close sidebar on click
     closeSidebar(); // Call closeSidebar function from SideBar component
   };
@@ -31,26 +31,27 @@ function Nav({ isHovered, setIsHovered, closeSidebar }) {
     <div className="">
       <ul className="flex flex-col gap-2">
         {navItems.map((item, index) => (
-          <li
-            key={index}
-            className={`relative flex items-center gap-3 hover:bg-[#9898be] hover:border-r-4 hover:border-[#3F51B5] transition-all duration-300 ${
-              currentPage === index + 1 ? "bg-[#9898be]" : ""
-            } ${
-              isHovered && currentPage === index + 1
-                ? "border-r-4 border-[#3F51B5]"
-                : ""
-            } h-10 rounded-sm px-3 hover:scale-105 transform`}
-            onClick={() => handleClick(index + 1)}
-          >
-            <item.icon size={25} />
-            <p
-              className={`absolute left-12 w-full ${
-                isHovered ? "opacity-100" : "opacity-0"
-              } transition-opacity duration-300 md:block`}
+          <NavLink key={index} to={item.text}>
+            <li
+              className={`relative flex items-center gap-3 hover:bg-[#9898be] hover:border-r-4 hover:border-[#3F51B5] transition-all duration-300 ${
+                location === item.text ? "bg-[#9898be]" : ""
+              } ${
+                isHovered && location === item.text
+                  ? "border-r-4 border-[#3F51B5]"
+                  : ""
+              } h-10 rounded-sm px-3 hover:scale-105 transform`}
+              onClick={() => handleClick(index + 1)}
             >
-              {item.text}
-            </p>
-          </li>
+              <item.icon size={25} />
+              <p
+                className={`absolute left-12 w-full ${
+                  isHovered ? "opacity-100" : "opacity-0"
+                } transition-opacity duration-300 md:block`}
+              >
+                {item.text}
+              </p>
+            </li>
+          </NavLink>
         ))}
       </ul>
     </div>
