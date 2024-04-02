c8a8a4fcf9e713d140cf26fcca657eef3c6d9c9f
brsekani <lawaltemidayo06@gmail.com>
2024-04-02 13:52:26 -0700

    Added the current page to the header

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,10 +1,13 @@
 import { FaRegBell } from "react-icons/fa";
 import faceImage from "/public/face image.avif";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation().pathname.slice(1);
+
   return (
     <div className="h-12 p-4 bg-[#1E1E1E] flex items-center justify-between">
-      <h1 className="text-xl font-bold capitalize">Dashboard</h1>
+      <h1 className="text-xl font-bold capitalize">{location}</h1>
       <div className="flex items-center gap-7">
         <FaRegBell size={20} />
         <img
