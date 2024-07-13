6cce517b2ebefe85fb7c8d15cf7d9756e1f9cfd1
brsekani <lawaltemidayo06@gmail.com>
2024-07-13 18:55:10 +0100

    Working on the goals

--- src/Modals/AddNewGoal.jsx
+++ src/Modals/AddNewGoal.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setShowAddNewGoal } from "../Features/uiSlice";
 import { GoGoal } from "react-icons/go";
 import { useForm, Controller } from "react-hook-form";
+import { PiCurrencyDollarSimpleBold } from "react-icons/pi";
 
 function AddNewGoal() {
   const darkMode = useSelector((state) => state.darkMode);
@@ -29,7 +30,15 @@ function AddNewGoal() {
     control,
     handleSubmit,
     formState: { errors },
+    register,
+    setValue,
   } = useForm();
+
+  const formatNumber = (value) => {
+    const parts = value.replace(/[^0-9]/g, "").split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+  };
 
   const onSubmit = (data) => {
     console.log(data);
@@ -57,33 +66,70 @@ function AddNewGoal() {
                 name="name"
                 control={control}
                 defaultValue=""
+                rules={{
+                  required: "Please enter the name of your goal",
+                  minLength: {
+                    value: 3,
+                    message: "Please enter a 3 characters",
+                  },
+                  //   pattern: {
+                  //     value: /^\d{10}$/,
+                  //     message: "Please enter a valid 10-digit account number",
+                  //   },
+                }}
                 render={({ field }) => (
                   <input
                     {...field}
                     type="text"
                     className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
                     placeholder="Enter goal name"
+                    maxLength={10}
                   />
                 )}
               />
+              {errors.name && (
+                <span className="text-xs text-red-500">
+                  {errors.name.message}
+                </span>
+              )}
             </div>
-            <div className="mb-4">
+            <div className="relative flex flex-col mb-4">
               <label className="block mb-1 text-sm font-medium">
                 Target Amount
               </label>
-              <Controller
-                name="targetAmount"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="number"
-                    className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                    placeholder="Enter target amount"
-                  />
-                )}
+              <input
+                className="w-full h-10 bg-transparent border border-white/1 pl-7"
+                placeholder="Enter Amount"
+                type="text"
+                inputMode="tel"
+                autoComplete="off"
+                maxLength={15}
+                {...register("amount", {
+                  required: "Enter an amount",
+                  validate: (value) => {
+                    const amount = parseFloat(value.replace(/,/g, ""));
+                    if (isNaN(amount)) return "Invalid amount";
+                    if (amount < 100) return "Amount must be at least $10";
+                    return true;
+                  },
+                })}
+                onChange={(e) => {
+                  const rawValue = e.target.value.replace(/,/g, "");
+                  const formattedValue = formatNumber(rawValue);
+
+                  setValue("amount", formattedValue, { shouldValidate: true });
+                }}
               />
+              <PiCurrencyDollarSimpleBold
+                color="rgb(0, 163, 255)"
+                size={25}
+                className="absolute top-8 left-1"
+              />
+              {errors.amount && (
+                <span className="text-xs text-red-500">
+                  {errors.amount.message}
+                </span>
+              )}
             </div>
             <button
               type="submit"
